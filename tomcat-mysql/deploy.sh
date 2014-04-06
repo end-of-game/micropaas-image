302f269eb976a862f8a4da080408c4362b041f6f
Nicolas <zepouet@gmail.com>
2014-04-06 13:25:49 +0200

    Suppresion nom statique du war
    On n'est plus lié à project.war et on prend comme contexte le nom de l'application

--- tomcat-mysql/deploy.sh
+++ tomcat-mysql/deploy.sh
@@ -1,12 +1,12 @@
-
 export TOMCAT_HOME=/opt/tomcat
-export WAR_FILE=/project.war
+export WAR_FILE=/deploy/$1
 
 sh $TOMCAT_HOME/bin/catalina.sh stop
+sleep 5
 
-rm $TOMCAT_HOME/webapps/ROOT.war
-rm -rf $TOMCAT_HOME/webapps/ROOT $TOMCAT_HOME/work/Catalina/localhost/_
+rm -rf $TOMCAT_HOME/webapps/$1*
+rm -rf $TOMCAT_HOME/work/Catalina/localhost/_
 
-cp $WAR_FILE $TOMCAT_HOME/webapps/ROOT.war
+cp $WAR_FILE $TOMCAT_HOME/webapps/$1
 
 sh $TOMCAT_HOME/bin/catalina.sh start
