19edb352dc8905fcbcd0156a6cfc159a4129392d
Nicolas <zepouet@gmail.com>
2014-04-06 14:17:59 +0200

    Update deploy.sh

--- tomcat-mysql/deploy.sh
+++ tomcat-mysql/deploy.sh
@@ -1,12 +1,13 @@
 export TOMCAT_HOME=/opt/tomcat
-export WAR_FILE=/deploy/$1
+export WAR_NAME=$1
+export WAR_PATH=/deploy/$1
 
 sh $TOMCAT_HOME/bin/catalina.sh stop
-sleep 5
+sleep 1
 
-rm -rf $TOMCAT_HOME/webapps/$1*
+rm -rf $TOMCAT_HOME/webapps/${WAR_NAME%.*ar}
 rm -rf $TOMCAT_HOME/work/Catalina/localhost/_
 
-cp $WAR_FILE $TOMCAT_HOME/webapps/$1
+cp $WAR_PATH $TOMCAT_HOME/webapps/$1
 
 sh $TOMCAT_HOME/bin/catalina.sh start
